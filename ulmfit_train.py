5cdcb9fb5f0a6acb8d79bc90eb853a341f6dfa5b
Piotr Niewinski <p.niewinski@edrone.me>
2021-05-04 13:51:02 +0200

    # Conflicts: #	lm/modelling_scripts #	lm/ulmfit_train.py #	recommendation/clickhouse-sync/save_day.py #	scraping/parse_skalnik.py

--- ulmfit_train.py
+++ ulmfit_train.py
@@ -6,7 +6,7 @@ import pickle
 from fastai.basics import *
 from fastai.callback.all import *
 from fastai.text.all import *
-from ulmfit_commons import lr_or_default, get_fastai_tensors
+from ulmfit_commons import lr_or_default, get_fastai_tensors, save_as_keras
 
 """
 Pre-train ULMFit on already tokenized and numericalized corpora.
@@ -90,44 +90,17 @@ def main(args):
         learner_obj = _run_pretraining(learner_obj, args)
     print("Saving the ULMFit model in FastAI format ...")
     learner_obj.save(os.path.join(args['save_path'], args['exp_name'])) # .pth will be added automatically
-    print("Saving the ULMFit model's weights into a pickle ...")
-    pickle.dump(learner_obj.model.state_dict(), open(os.path.join(args['save_path'], f"{args['exp_name']}_state_dict.p"), 'wb'))
-    if args.get('export_to_tf'):
-        print("Saving the ULMFit model as Keras Sequential object")
-        save_as_keras(learner_obj.model.state_dict(), args)
+    # print("Saving the ULMFit model's weights into a pickle ...")
+    # pickle.dump(learner_obj.model.state_dict(), open(os.path.join(args['save_path'], f"{args['exp_name']}_state_dict.p"), 'wb'))
+    if args.get('export_to_tf2'):
+        print("Saving the ULMFit model to a Keras checkpoint...")
+        save_as_keras(learner_obj.model.state_dict(),
+                args.get['exp_name'],
+                args.get['save_path'],
+                'on',
+                args.get['spm_model_file'])
+        print("Done")
     return learner_obj
-
-def save_as_keras(state_dict, args):
-    """
-    Creates an ULMFit inference model using Keras layers and copies weights from FastAI's learner.model.state_dict() there.
-
-    There are many explicit constants in this function, which is intentional. The numbers 400, 1152 and 3 layers refer
-    to the paper's implementation of ULMFit in FastAI.
-
-    """
-
-    import tensorflow as tf
-    kmodel = tf.keras.models.Sequential()
-    kmodel.add(tf.keras.layers.Input(shape=[None], dtype=tf.int32, ragged=True)) # ragged tensors = variable input length!
-    kmodel.add(tf.keras.layers.Embedding(args['vocab_size'], 400, name='ulmfit_embedz'))
-    kmodel.add(tf.keras.layers.LSTM(1152, return_sequences=True, name='ulmfit_lstm1'))
-    kmodel.add(tf.keras.layers.LSTM(1152, return_sequences=True, name='ulmfit_lstm2'))
-    kmodel.add(tf.keras.layers.LSTM(400, return_sequences=True, name='ulmfit_lstm3'))
-    kmodel.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(args['vocab_size'], activation='linear'), name='lm_head')) # originally: linear
-
-    kmodel.get_layer('ulmfit_embedz').set_weights([state_dict['0.encoder.weight'].cpu().numpy()])
-    kmodel.get_layer('ulmfit_lstm1').set_weights([state_dict['0.rnns.0.module.weight_ih_l0'].cpu().numpy().T,
-                                                  state_dict['0.rnns.0.weight_hh_l0_raw'].cpu().numpy().T,
-                                                  state_dict['0.rnns.0.module.bias_ih_l0'].cpu().numpy()*2])
-    kmodel.get_layer('ulmfit_lstm2').set_weights([state_dict['0.rnns.1.module.weight_ih_l0'].cpu().numpy().T,
-                                                  state_dict['0.rnns.1.weight_hh_l0_raw'].cpu().numpy().T,
-                                                  state_dict['0.rnns.1.module.bias_ih_l0'].cpu().numpy()*2])
-    kmodel.get_layer('ulmfit_lstm3').set_weights([state_dict['0.rnns.2.module.weight_ih_l0'].cpu().numpy().T,
-                                                  state_dict['0.rnns.2.weight_hh_l0_raw'].cpu().numpy().T,
-                                                  state_dict['0.rnns.2.module.bias_ih_l0'].cpu().numpy()*2])
-    kmodel.get_layer('lm_head').set_weights([state_dict['1.decoder.weight'].cpu().numpy().T,
-                                             state_dict['1.decoder.bias'].cpu().numpy()])
-    kmodel.save(os.path.join(args['save_path'], f"{args['exp_name']}_tf.hdf5"))
 
 if __name__ == "__main__":
     argz = argparse.ArgumentParser()
@@ -136,7 +109,8 @@ if __name__ == "__main__":
                       "into one big stream!")
     argz.add_argument("--pretokenized-valid", required=False, help="Path to a pretokenized and numericalized validation corpus. " \
                       "Same tokenization rules apply as for the training corpus.")
-    argz.add_argument("--pretrained-model", required=False, help="Path to a pretrained FastAI/PyTorch model OR a state dict. " \
+    argz.add_argument("--spm-model-file", required=True, help="Fixme: needed only to instantiate Keras saver") \
+    argz.add_argument("--pretrained-model", required=False, help="Path to a pretrained FastAI/PyTorch model. " \
                       "Use this as input to Keras conversion (state dict) or if you want to resume pretraining (pth model).")
     argz.add_argument("--min-seq-len", default=10, type=int, help="Minimal sentence length in the original corpus")
     argz.add_argument("--max-seq-len", default=40, type=int, help="Maximal sequence length in a training batch. This is the same as BPTT.")
@@ -150,18 +124,8 @@ if __name__ == "__main__":
                                                                       "Only used for finetuning starting from the second epoch.") # 5e-4
     argz.add_argument("--save-path", required=True, help="Path where the outputs will be saved")
     argz.add_argument("--exp-name", required=True, help="Experiment name")
-    argz.add_argument("--export-to-numpy", action='store_true', help="Will save an additional pickle containing layer weights "\
-                                                                     "as Numpy arrays. These can be loaded into a TensorFlow inference model.")
-    argz.add_argument("--export-to-tf", action='store_true', help="Will save an additional hdf5 file containing Keras-loadable "\
+    argz.add_argument("--export-to-tf2", action='store_true', help="Will save an additional TF checkpoint containing Keras-loadable "\
                                                                   "ULMFit model with LM head.")
 
     argz = vars(argz.parse_args())
-    if argz.get('pretrained_model') is not None and argz.get('export_to_tf') is True and argz.get('pretokenized_train') is None:
-        state_dict = pickle.load(open(argz['pretrained_model'], 'rb'))
-        save_as_keras(state_dict, argz)
-    elif argz.get('pretokenized_train') is None:
-        print("Please provide a path to pretokenized and numericalized corpus")
-        print(argz)
-        exit(1)
-    else:
-        main(argz)
+    main(argz)
