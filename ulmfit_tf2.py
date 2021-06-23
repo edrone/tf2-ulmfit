Batch normalization and ConcatPooler fixes
 * reorder dropouts
 * reorder BatchNorm before activations
 * ensure ConcatPooler consistency with TF RNN API

(tf.keras.layers.LSTMCell wrapped around tf.keras.layers.RNN is not the
same thing as tf.keras.layers.LSTM after all... The former propagates
the last hidden state over padding mask, the latter does not.)

ulmfit_tf2.py
@@ -724,13 +724,29 @@ class ConcatPooler(tf.keras.layers.Layer):
         self._supports_ragged_inputs = False # for compatibility with TF 2.2
     
     def build(self, input_shape):
-        print(">>>> INSIDE BUILD / RaggedConcatPooler <<<< ")
+        print(">>>> INSIDE BUILD / ConcatPooler <<<< ")
     
-    def call(self, inputs, training=None): # inputs is a fixed-length tensor
-        last_hidden_state = inputs[:, -1, :] # nevermind padding - Keras mask ensures the last meaningful value is repeated until the sequence's end
-        max_pooled = tf.math.reduce_max(inputs, axis=1)
-        mean_pooled = tf.math.reduce_mean(inputs, axis=1)
-        ret = tf.concat([last_hidden_state, max_pooled, mean_pooled], axis=1)
+    def call(self, inputs, training=None, mask=None): # inputs is a fixed-length tensor
+        # We cannot use the line below. An tf.keras.layers.LSTMCell wrapped around tf.keras.layers.RNN propagates the last hidden
+        # state to the end of the sequence when it encounters padding, whereas a tf.keras.layers.LSTM will insert zeroes.
+        # This is quite a gotcha, so we have to compute the position of the last hidden state from Keras mask instead.
+        #last_hidden_states = inputs[:, -1, :]
+
+        surely_masked_inputs = tf.where(tf.expand_dims(mask, axis=-1), inputs, tf.zeros_like(inputs)) # always zero on padding
+        surely_maxable_inputs = tf.where(tf.expand_dims(mask, axis=-1), inputs, tf.fill(tf.shape(inputs), -np.inf)) # always minus infinity on padding
+        num_indices = tf.reduce_sum(tf.cast(mask, dtype=tf.int32), axis=1) # number of tokens
+        last_indices = num_indices - 1 # last token index
+        batch_index = tf.range(tf.shape(inputs)[0])
+        last_hidden_states = tf.gather_nd(inputs, tf.stack([batch_index, last_indices], axis=1))
+
+        #mean_pooled = tf.math.reduce_mean(inputs, axis=1)
+        summed = tf.reduce_sum(surely_masked_inputs, axis=1)
+        mean_pooled = summed / tf.expand_dims(tf.cast(num_indices, dtype=tf.float32), axis=-1)
+
+        #max_pooled = tf.math.reduce_max(inputs, axis=1)
+        max_pooled = tf.math.reduce_max(surely_maxable_inputs, axis=1)
+
+        ret = tf.concat([last_hidden_states, max_pooled, mean_pooled], axis=1)
         return ret
 
     def compute_output_shape(self, input_shape):

ulmfit_tf2_heads.py
@@ -151,21 +151,19 @@ def ulmfit_document_classifier(*, model_type, pretrained_encoder_weights, num_cl
     else:
         rpooler = ConcatPooler(name="ConcatPooler")(ulmfit_rnn_encoder.output)
 
-    #if with_batch_normalization is True:
-    #    bnorm1 = tf.keras.layers.BatchNormalization(epsilon=1e-05, momentum=0.1, scale=False, center=False)(rpooler)
-    #    drop1 = tf.keras.layers.Dropout(0.2)(bnorm1)
-    #else:
-    #    drop1 = tf.keras.layers.Dropout(0.2)(rpooler)
-    #fc1 = tf.keras.layers.Dense(50, activation='relu')(drop1)
-    fc1 = tf.keras.layers.Dense(50, activation='relu')(rpooler)
-    #if with_batch_normalization is True:
-    #    bnorm2 = tf.keras.layers.BatchNormalization(epsilon=1e-05, momentum=0.1, scale=False, center=False)(fc1)
-    #    drop2 = tf.keras.layers.Dropout(0.1)(bnorm2)
-    #else:
-    #    drop2 = tf.keras.layers.Dropout(0.1)(fc1)
-    #fc_final = tf.keras.layers.Dense(num_classes, activation=activation)(drop2)
-    fc_final = tf.keras.layers.Dense(num_classes, activation=activation)(fc1)
-    #fc_final = tf.keras.layers.Dense(num_classes, activation=activation)(fc1)
+    drop_pooler = tf.keras.layers.Dropout(0.2)(rpooler)
+    if with_batch_normalization is True:
+        bnorm_pooler = tf.keras.layers.BatchNormalization(epsilon=1e-05, momentum=0.1, scale=False, center=False)(drop_pooler)
+        fc1 = tf.keras.layers.Dense(50, activation='linear')(bnorm_pooler)
+        bnorm1 = tf.keras.layers.BatchNormalization(epsilon=1e-05, momentum=0.1, scale=False, center=False)(fc1)
+        relu1 = tf.keras.layers.ReLU()(bnorm1)
+        drop2 = tf.keras.layers.Dropout(0.1)(relu1)
+        fc_final = tf.keras.layers.Dense(num_classes, activation='softmax')(drop2)
+    else:
+        fc1 = tf.keras.layers.Dense(50, activation='relu')(drop_pooler)
+        drop2 = tf.keras.layers.Dropout(0.1)(fc1)
+        fc_final = tf.keras.layers.Dense(num_classes, activation=activation)(drop2)
+
     document_classifier_model = tf.keras.models.Model(inputs=ulmfit_rnn_encoder.inputs, outputs=fc_final)
     return document_classifier_model, hub_object
 